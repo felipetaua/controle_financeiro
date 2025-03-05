c50c3daf92b832e2c12e94a8c6497fbc2d1d062e
felipetaua <taua.felipee@gmail.com>
2025-03-05 10:41:45 -0300

    transações data

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
 
-var myData = [
+var transationsData = [
   {
     'icon': Icons.fastfood,
+    'color': Colors.yellow[700],
     'name': 'Food',
     'totalAlmount': 'R\$-45,00',
     'date': 'Hoje',
   },
   {
     'icon': Icons.store,
+    'color': Colors.red[700],
     'name': 'Store',
-    'totalAlmount': 'R\$-95,00',
+    'totalAlmount': '-R\$95,00',
     'date': 'Hoje',
   },
   {
     'icon': Icons.diamond_outlined,
+    'color': Colors.purple[700],
     'name': 'Shopping',
-    'totalAlmount': 'R\$255,00',
+    'totalAlmount': '-R\$255,00',
     'date': 'Hoje',
+  },
+  {
+    'icon': Icons.health_and_safety_rounded,
+    'color': Colors.green[700],
+    'name': 'Saúde',
+    'totalAlmount': '-R\$450,00',
+    'date': 'Anteontem',
+  },
+  {
+    'icon': Icons.health_and_safety_rounded,
+    'color': Colors.blue[400],
+    'name': 'Viajem',
+    'totalAlmount': '-R\$850,00',
+    'date': 'Mês passado',
   }
 ];
