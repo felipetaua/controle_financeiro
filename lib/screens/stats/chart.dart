7c2e0341a9aebfc6b022678e0d25c03163d165e2
felipetaua <taua.felipee@gmail.com>
2025-03-06 09:08:38 -0300

    adicionando graficos da bibllioteca flutter

---
+++ lib/screens/stats/chart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyChart extends StatefulWidget {
+  const MyChart({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/stats/stats.dart
+++ lib/screens/stats/stats.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class StatScreen extends StatelessWidget {
@@ -7,12 +8,21 @@ class StatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
         child: Column(
           children: [
             Text(
               'Transações',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              // color: Colors.red,
+              child: BarChart(BarChartData()),
             )
           ],
         ),
