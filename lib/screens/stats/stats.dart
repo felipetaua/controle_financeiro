362c33362c1b0b980b97f5ff9ce79c4a85925104
felipetaua <taua.felipee@gmail.com>
2025-03-08 19:23:01 -0300

    ajustes

--- lib/screens/stats/stats.dart
+++ lib/screens/stats/stats.dart
@@ -1,6 +1,5 @@
-import 'package:fl_chart/fl_chart.dart';
+//import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-
 import 'chart.dart';
 
 class StatScreen extends StatelessWidget {
