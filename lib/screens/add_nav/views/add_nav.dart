f22d61e9a49beccd15b6a3ad0e922ee78e4b173a
felipetaua <taua.felipee@gmail.com>
2025-03-09 22:53:37 -0300

    criando input de valor, adicionando input de categoria e inserir a data

--- lib/screens/add_nav/views/add_nav.dart
+++ lib/screens/add_nav/views/add_nav.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class addNavButton extends StatelessWidget {
   const addNavButton({super.key});
@@ -21,17 +22,70 @@ class addNavButton extends StatelessWidget {
                 "Adicione seus gastos",
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
               ),
-              TextFormField(),
               const SizedBox(
-                height: 12,
+                height: 16,
               ),
-              TextFormField(),
-              const SizedBox(
-                height: 12,
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: const Icon(
+                        FontAwesomeIcons.dollarSign,
+                        size: 16,
+                        color: Colors.grey,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(32),
+                          borderSide: BorderSide.none)),
+                ),
               ),
-              TextFormField(),
               const SizedBox(
-                height: 12,
+                height: 16,
+              ),
+              TextFormField(
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      FontAwesomeIcons.list,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    hintText: ('Categoria'),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide.none)),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                textAlignVertical: TextAlignVertical.center,
+                onTap: () {
+                  showDatePicker(
+                      context: context,
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime.now().add(Duration(days: 365)));
+                },
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      FontAwesomeIcons.calendar,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    hintText: ('Data'),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide.none)),
+              ),
+              const SizedBox(
+                height: 16,
               ),
               TextButton(onPressed: () {}, child: Text('Salvar'))
             ],
