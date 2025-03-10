781f77b60728d0f19e7b41c86d6ed7f8d3bec7a8
felipetaua <taua.felipee@gmail.com>
2025-03-09 22:17:46 -0300

    adicionando click ao botão inicial iniciando o formulário

--- lib/screens/add_nav/views/add_nav.dart
+++ lib/screens/add_nav/views/add_nav.dart
@@ -5,6 +5,39 @@ class addNavButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Adicione seus gastos",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+              TextFormField(),
+              const SizedBox(
+                height: 12,
+              ),
+              TextFormField(),
+              const SizedBox(
+                height: 12,
+              ),
+              TextFormField(),
+              const SizedBox(
+                height: 12,
+              ),
+              TextButton(onPressed: () {}, child: Text('Salvar'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
