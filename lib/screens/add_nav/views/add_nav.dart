693f05252d410b8ed65e8706d2a4eed3598d15ce
felipetaua <taua.felipee@gmail.com>
2025-03-18 11:03:50 -0300

    att

--- lib/screens/add_nav/views/add_nav.dart
+++ lib/screens/add_nav/views/add_nav.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:intl/intl_browser.dart';
+import 'package:intl/intl.dart';
 
 class addNavButton extends StatefulWidget {
   const addNavButton({super.key});
