9a49bf033eca446068ae2bbddc471b21e56ed4ca
felipetaua <taua.felipee@gmail.com>
2025-04-01 12:00:59 -0300

    ao abrir o aplicativo com banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:controle_financeiro/app.dart';
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
 }
