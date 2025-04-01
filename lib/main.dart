43dab5c83b0c70a2949db1dfc25f4744a29dd001
felipetaua <taua.felipee@gmail.com>
2025-04-01 13:22:58 -0300

    arrumando os erros de importação

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:controle_financeiro/app.dart';
-import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
+import 'package:controle_financeiro/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'simple_bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
